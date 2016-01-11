8cc30cb9584c86cd2e91dfc0abc5a8d30bfdb484
Hilke Ros <hilkeros@gmail.com>
2016-01-11 21:04:57 +0100

    added lots of content

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,8 +4,86 @@
 
 var SchrijfdagApp = angular.module('SchrijfdagApp', ['ngSanitize', 'ui.bootstrap']);
 
-SchrijfdagApp.controller('WorkshopsCtrl', ['$scope', '$http', function($scope, $http) {
-  $http.get('workshops.json').success(function(data) {
+SchrijfdagApp.controller('VMWorkshopsCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('workshopsVM.json').success(function(data) {
+    $scope.workshops = data;
+  });
+
+  $scope.fillerClass = "col-md-6";
+  $scope.workshopContainerClass = "col-md-6";
+  $scope.unselectedPanel ="col-md-6";
+  $scope.selectedWorkshop = null;
+
+  $scope.toggleDetail = function(workshop){
+    if ($scope.selectedWorkshop === workshop) {
+      $scope.selectedWorkshop = null;
+      $scope.fillerClass = "col-md-6";
+      $scope.workshopContainerClass = "col-md-6";
+      $scope.unselectedPanel ="col-md-6";
+    } else {
+      $scope.selectedWorkshop = workshop;
+      $scope.fillerClass = "col-md-3";
+      $scope.workshopContainerClass = "col-md-9";
+      $scope.unselectedPanel ="col-md-4";
+    }
+  }
+
+}]);
+
+SchrijfdagApp.controller('NMWorkshopsCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('workshopsNM.json').success(function(data) {
+    $scope.workshops = data;
+  });
+
+  $scope.fillerClass = "col-md-6";
+  $scope.workshopContainerClass = "col-md-6";
+  $scope.unselectedPanel ="col-md-6";
+  $scope.selectedWorkshop = null;
+
+  $scope.toggleDetail = function(workshop){
+    if ($scope.selectedWorkshop === workshop) {
+      $scope.selectedWorkshop = null;
+      $scope.fillerClass = "col-md-6";
+      $scope.workshopContainerClass = "col-md-6";
+      $scope.unselectedPanel ="col-md-6";
+    } else {
+      $scope.selectedWorkshop = workshop;
+      $scope.fillerClass = "col-md-3";
+      $scope.workshopContainerClass = "col-md-9";
+      $scope.unselectedPanel ="col-md-4";
+    }
+  }
+
+}]);
+
+SchrijfdagApp.controller('VMLezingenCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('lezingenVM.json').success(function(data) {
+    $scope.workshops = data;
+  });
+
+  $scope.fillerClass = "col-md-6";
+  $scope.workshopContainerClass = "col-md-6";
+  $scope.unselectedPanel ="col-md-6";
+  $scope.selectedWorkshop = null;
+
+  $scope.toggleDetail = function(workshop){
+    if ($scope.selectedWorkshop === workshop) {
+      $scope.selectedWorkshop = null;
+      $scope.fillerClass = "col-md-6";
+      $scope.workshopContainerClass = "col-md-6";
+      $scope.unselectedPanel ="col-md-6";
+    } else {
+      $scope.selectedWorkshop = workshop;
+      $scope.fillerClass = "col-md-3";
+      $scope.workshopContainerClass = "col-md-9";
+      $scope.unselectedPanel ="col-md-4";
+    }
+  }
+
+}]);
+
+SchrijfdagApp.controller('NMLezingenCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('lezingenNM.json').success(function(data) {
     $scope.workshops = data;
   });
 
