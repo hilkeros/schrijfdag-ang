26f6453707db17f5379157b4ac9a3b69c7e3f98a
Hilke Ros <hilkeros@gmail.com>
2016-01-13 15:20:14 +0100

    everyhting in panels and json

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -108,6 +108,136 @@ SchrijfdagApp.controller('NMLezingenCtrl', ['$scope', '$http', function($scope, 
 
 }]);
 
+SchrijfdagApp.controller('VMInfosessiesCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('infosessiesVM.json').success(function(data) {
+    $scope.workshops = data;
+  });
+
+  $scope.fillerClass = "col-md-6";
+  $scope.workshopContainerClass = "col-md-6";
+  $scope.unselectedPanel ="col-md-6";
+  $scope.selectedWorkshop = null;
+
+  $scope.toggleDetail = function(workshop){
+    if ($scope.selectedWorkshop === workshop) {
+      $scope.selectedWorkshop = null;
+      $scope.fillerClass = "col-md-6";
+      $scope.workshopContainerClass = "col-md-6";
+      $scope.unselectedPanel ="col-md-6";
+    } else {
+      $scope.selectedWorkshop = workshop;
+      $scope.fillerClass = "col-md-3";
+      $scope.workshopContainerClass = "col-md-9";
+      $scope.unselectedPanel ="col-md-4";
+    }
+  }
+
+}]);
+
+SchrijfdagApp.controller('NMInfosessiesCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('infosessiesNM.json').success(function(data) {
+    $scope.workshops = data;
+  });
+
+  $scope.fillerClass = "col-md-6";
+  $scope.workshopContainerClass = "col-md-6";
+  $scope.unselectedPanel ="col-md-6";
+  $scope.selectedWorkshop = null;
+
+  $scope.toggleDetail = function(workshop){
+    if ($scope.selectedWorkshop === workshop) {
+      $scope.selectedWorkshop = null;
+      $scope.fillerClass = "col-md-6";
+      $scope.workshopContainerClass = "col-md-6";
+      $scope.unselectedPanel ="col-md-6";
+    } else {
+      $scope.selectedWorkshop = workshop;
+      $scope.fillerClass = "col-md-3";
+      $scope.workshopContainerClass = "col-md-9";
+      $scope.unselectedPanel ="col-md-4";
+    }
+  }
+
+}]);
+
+SchrijfdagApp.controller('FeedbackCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('feedback.json').success(function(data) {
+    $scope.workshops = data;
+  });
+
+  $scope.fillerClass = "col-md-6";
+  $scope.workshopContainerClass = "col-md-6";
+  $scope.unselectedPanel ="col-md-6";
+  $scope.selectedWorkshop = null;
+
+  $scope.toggleDetail = function(workshop){
+    if ($scope.selectedWorkshop === workshop) {
+      $scope.selectedWorkshop = null;
+      $scope.fillerClass = "col-md-6";
+      $scope.workshopContainerClass = "col-md-6";
+      $scope.unselectedPanel ="col-md-6";
+    } else {
+      $scope.selectedWorkshop = workshop;
+      $scope.fillerClass = "col-md-3";
+      $scope.workshopContainerClass = "col-md-9";
+      $scope.unselectedPanel ="col-md-4";
+    }
+  }
+
+}]);
+
+SchrijfdagApp.controller('EnOokNogCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('enooknog.json').success(function(data) {
+    $scope.workshops = data;
+  });
+
+  $scope.fillerClass = "col-md-6";
+  $scope.workshopContainerClass = "col-md-6";
+  $scope.unselectedPanel ="col-md-6";
+  $scope.selectedWorkshop = null;
+
+  $scope.toggleDetail = function(workshop){
+    if ($scope.selectedWorkshop === workshop) {
+      $scope.selectedWorkshop = null;
+      $scope.fillerClass = "col-md-6";
+      $scope.workshopContainerClass = "col-md-6";
+      $scope.unselectedPanel ="col-md-6";
+    } else {
+      $scope.selectedWorkshop = workshop;
+      $scope.fillerClass = "col-md-3";
+      $scope.workshopContainerClass = "col-md-9";
+      $scope.unselectedPanel ="col-md-4";
+    }
+  }
+
+}]);
+
+SchrijfdagApp.controller('OntdekCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('ontdek.json').success(function(data) {
+    $scope.workshops = data;
+  });
+
+  $scope.fillerClass = "col-md-6";
+  $scope.workshopContainerClass = "col-md-6";
+  $scope.unselectedPanel ="col-md-6";
+  $scope.selectedWorkshop = null;
+
+  $scope.toggleDetail = function(workshop){
+    if ($scope.selectedWorkshop === workshop) {
+      $scope.selectedWorkshop = null;
+      $scope.fillerClass = "col-md-6";
+      $scope.workshopContainerClass = "col-md-6";
+      $scope.unselectedPanel ="col-md-6";
+    } else {
+      $scope.selectedWorkshop = workshop;
+      $scope.fillerClass = "col-md-3";
+      $scope.workshopContainerClass = "col-md-9";
+      $scope.unselectedPanel ="col-md-4";
+    }
+  }
+
+}]);
+
 SchrijfdagApp.controller('NavBarCtrl', function($scope) {
   $scope.isCollapsed = true;
 });
